fed662ee98cca321d52d4364213966ba6478c443
pixochi <kozak.jakub55@gmail.com>
2019-08-25 13:00:46 +1000

    Extend comments for Services

--- CloudApplicationDevTask1/CloudApplicationDevTask1/Services/AllocationService.cs
+++ CloudApplicationDevTask1/CloudApplicationDevTask1/Services/AllocationService.cs
@@ -13,6 +13,12 @@ namespace CloudApplicationDevTask1.Services
         /// <summary>
         /// Computes the energy consumed by a task allocated to an NGHz processor
         /// </summary>
+        /// <returns>
+        /// The energy consumed by a task
+        /// </returns>
+        /// <param name="coefficients">Coefficients of a quadratic formulas</param>
+        /// <param name="frequency">Processor frequency</param>
+        /// <param name="runtime">Runtime of the task</param>
         public static float GetEnergyConsumedPerTask(List<float> coefficients, float frequency, float runtime)
         {
             return (coefficients[2] * frequency * frequency + coefficients[1] * frequency + coefficients[0]) * runtime;
@@ -21,6 +27,11 @@ namespace CloudApplicationDevTask1.Services
         /// <summary>
         /// Computes the energy consumed by an allocation
         /// </summary>
+        /// <returns>
+        /// The energy consumed by an allocation
+        /// </returns>
+        /// <param name="configFileContent">Content of a configuration file</param>
+        /// <param name="allocation">Allocation of tasks on processors</param>
         public static float GetTotalEnergyConsumed(string configFileContent, List<List<bool>> allocation)
         {
             List<float> coefficients = ConfigFileParser.GetCoefficients(configFileContent);
@@ -62,6 +73,12 @@ namespace CloudApplicationDevTask1.Services
         /// <summary>
         /// Computes the runtime of a task allocated to an NGHz processor
         /// </summary>
+        /// <returns>
+        /// The runtime of a task
+        /// </returns>
+        /// <param name="referenceFrequency">A frequency used to determine task runtimes in a configuration file</param>
+        /// <param name="runtime">Runtime determined with the reference frequency</param>
+        /// <param name="frequency">Frequency of an actual processor to which a task is assigned</param>
         public static float GetTaskRuntime(float referenceFrequency, float runtime, float frequency)
         {
             return runtime * (referenceFrequency / frequency);
@@ -70,6 +87,11 @@ namespace CloudApplicationDevTask1.Services
         /// <summary>
         /// Computes the runtime of an allocation
         /// </summary>
+        /// <returns>
+        /// The runtime of an allocation
+        /// </returns>
+        /// <param name="configFileContent">Content of a configuration file</param>
+        /// <param name="allocation">A single allocation of tasks on processors</param>
         public static float GetAllocationRuntime(string configFileContent, List<List<bool>> allocation)
         {
             List<float> processorFrequencies = ConfigFileParser.GetProcessorFrequencies(configFileContent);
@@ -108,6 +130,12 @@ namespace CloudApplicationDevTask1.Services
         /// <summary>
         /// Checks if the runtime of an allocation exceeds the maximum program duration
         /// </summary>
+        /// <returns>
+        /// An empty string is the runtime is valid,
+        /// an error message if invalid
+        /// </returns>
+        /// <param name="configFileContent">Content of a configuration file</param>
+        /// <param name="allocationRuntime">Runtime of an allocation</param>
         public static string IsAllocationRuntimeValid(string configFileContent, float allocationRuntime)
         {
             string errorMsg = "Runtime of the given allocation exceeds maximum duration of the program.";
